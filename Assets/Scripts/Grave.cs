48383bbafc0387549ebccca42e8398cf0987dadd
Kasperi <Kassuz@users.noreply.github.com>
2016-11-26 13:52:08 +0200

    Player can move and resurrect

---
+++ Assets/Scripts/Grave.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grave : MonoBehaviour
+{
+    [SerializeField] private GameObject zombiePrefab;
+
+    private bool isEmpty = false;
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player" && !isEmpty)
+        {
+            PlayerMovement pM = col.GetComponent<PlayerMovement>();
+            if (pM.IsResurrecting)
+            {
+                isEmpty = true;
+                StartCoroutine(SpawnZombie());                
+            }
+        }
+    }
+
+    private IEnumerator SpawnZombie()
+    {
+        yield return new WaitForSeconds(1f);
+        Instantiate(zombiePrefab, transform.position, Quaternion.identity);
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed;
 
+    public bool IsResurrecting { get; private set; }
+
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            IsResurrecting = true;
+        } 
+        else
+        {
+            IsResurrecting = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxisRaw("Vertical");
+        float inputY = Input.GetAxisRaw("Horizontal");
+
+        Move(-inputX, inputY);
+
+    }
+
+    private void Move(float x, float y)
+    {
+        if (x != 0 || y != 0)
+        {
+            Vector3 movement = new Vector3(x, 0f, y);
+
+            movement = movement.normalized * speed * Time.deltaTime;
+
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+            rb.MovePosition(transform.position + movement);
+        }
+    }
 
 }
