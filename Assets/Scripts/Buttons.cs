2913146461c7b83acc92e6bfed6459423bdda0f8
Kasperi <Kassuz@users.noreply.github.com>
2016-11-27 11:48:18 +0200

    Finalized game

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 
 public class Buttons : MonoBehaviour
 {
+    public GameObject popUp;
+    public Text titleText;
+    public Text creditsText;
+    public Text helpText;
 
     public void StartGame()
     {
@@ -14,5 +19,28 @@ public class Buttons : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        ClosePopUp();
+        popUp.SetActive(true);
+        titleText.text = "Credits";
+        creditsText.enabled = true;
+    }
+
+    public void ClosePopUp()
+    {
+        popUp.SetActive(false);
+        creditsText.enabled = false;
+        helpText.enabled = false;
+    }
+
+    public void Help()
+    {
+        ClosePopUp();
+        popUp.SetActive(true);
+        titleText.text = "Help";
+        helpText.enabled = true;
     }
 }
